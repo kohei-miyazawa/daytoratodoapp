7037015c3b04d204ca97400b2c165bedae2aaccf
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-09-15 14:28:01 +0900

    Board一覧画面作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,20 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
   end
 
   def new
+    @board = current_user.boards.build
   end
 
   def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path, notice: 'boardを作成しました'
+    else
+      flash.now[:error] = 'boradの作成に失敗しました'
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +25,9 @@ class BoardsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def board_params
+      params.require(:board).permit(:name, :description)
+    end
 end
