cf0d2b09b931cc23831022fd3cc2ba34a8e0643b
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-09-15 15:10:32 +0900

    エラーメッセージの出力実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @boards = Board.all
+    @boards = Board.all.order(updated_at: :desc)
   end
 
   def new
@@ -20,9 +20,17 @@ class BoardsController < ApplicationController
   end
 
   def edit
+    @board = current_user.boards.find(params[:id])
   end
 
   def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to root_path, notice: '更新しました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
   end
 
   def destroy
