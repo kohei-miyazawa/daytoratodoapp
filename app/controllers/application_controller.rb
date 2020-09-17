3b83d439ab1e23374d3533be72ad5531c5e87dfc
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-09-17 13:24:55 +0900

    アバターの表示設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    # deviseのpermitted_parameterを追加する
+    def configure_permitted_parameters
+      # sign_upを追加
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar] )
+    end
 end
